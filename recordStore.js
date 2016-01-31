62fc0cf3df82dabc4302e373a0aeb17617fa036a
User <user@Users-MBP-7.home>
2016-01-31 20:04:03 +0000

    all basics working

--- recordStore.js
+++ recordStore.js
@@ -18,20 +18,109 @@ Record.prototype = {
 
 }
 
-
-
-
 // Create a RecordStore that has a name, city and multiple records in it's inventory
-// Give the RecordStore a balance i.e. cash in bank.
-// Add some records to your RecordStore.
-// Create a method that lists the inventory.
-// Create a method so that the RecordStore can sell a record. Adjust the cash in bank to take into account the price of the record sold
-// Create a method that reports on the financial situation of the store. Cash and value of inventory.
-// Create a RecordCollector (or customer) constructor who can buy and sell records.
+
+var Store = function(name, city){
+  this.name = name;
+  this.city = city;
+  this.records = {};
+  this.profit = 0;
+  this.stockValue = 0;
+}
+
+Store.prototype = {
+  addRecord:function(record, quantity){
+    var key = record.title
+    var quantity = quantity || 0;
+
+    if(this.records[key]){
+    this.records[key].amount += quantity;  
+    }else{
+    this.records[key] = {album:record, amount:quantity} 
+    
+    }//close of if statement
+    this.stockValue += (quantity * this.records[key].album.price);
+
+},//close of function
+  listInventory:function(){
+    inventory = [];
+    for(var record in this.records){
+        inventoryObject = {};
+
+        inventoryObject[record] = this.records[record].amount;
+
+
+        // inventoryObject = {record: this.records[record].amount}
+
+        inventory.push(inventoryObject);
+    }
+    return inventory;
+  },
+
+  sellRecord:function(record){
+    var key = record.title
+      if(this.records[key]){ 
+          if(this.records[key].amount > 0){
+            this.records[key].amount-= 1;
+            this.profit += this.records[key]["album"].price;
+            this.stockValue -= this.records[key]["album"].price;
+          }else{
+            console.log("Sorry out of stock")
+          }
+      }else{
+          console.log("This item is not known")
+      }
+    
+  },
+
+  financialStatement:function(){
+    fianceObject = {};
+    fianceObject["takings"] = this.profit;
+    fianceObject["stockValue"] = this.stockValue;
+    fianceObject["inventory"] = this.listInventory();
+    console.log(fianceObject)
+    return fianceObject
+  }
+
+}//close of Store.prototype
+
+
+//Record Collector Stuff //
+
+var Customer = function(name, cash){
+  this.name = name;
+  this.cash = cash;
+  this.myRecords = {};
+}
+
+Customer.prototype = {
+  buyRecord: function(record, store){
+    var key = record.title;
+    if(store.records[key]){
+      this.myRecords[key] = {from:store.name, details:record, purchasedOn:new Date().toDateString()};
+      store.sellRecord(record);
+      this.cash -= record.price;
+    }else{
+      console.log("Record not available at this store");
+    }
+  },
+
+  getPaid: function(payment){
+    this.cash += payment;
+  },
+
+
+}
+
+
+
+
 // Use TDD all the way through!
 
 
 
 module.exports = {
-  recordConstructor: Record
+  recordConstructor: Record,
+  storeConstructor: Store,
+  customerConstructor: Customer
 }
--- recordStore_test.js
+++ recordStore_test.js
@@ -5,6 +5,8 @@ var expect = chai.expect;
 
 var imported = require('./recordStore');
 var Record = imported.recordConstructor;
+var Store = imported.storeConstructor;
+var Customer = imported.customerConstructor;
 
 describe('My JS Record Shop', function(){
   it('Can I add records and access the functions', function(){
@@ -25,5 +27,121 @@ describe('My JS Record Shop', function(){
     assert.deepEqual(["Gonna be a beautiful night"], record1.tracks);
 
   });
+
+  it('Can I create a record store and get it to display the store info', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    assert.equal("Selectadisc", store1.name);
+    assert.equal("Nottingham", store1.city);
+    assert.equal(0, store1.profit);
+    assert.equal(0, store1.stockValue);
+    assert.deepEqual({}, store1.records);
+
+  });
+
+  it('Can I add a record to the store and access the information', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    var record1 = new Record("Sign of the times", "Prince", 10);
+    var record2 = new Record("Controversy", "Prince", 20);
+    record1.addTrack("Gonna be a beautiful night")
+    store1.addRecord(record1, 2);
+    store1.addRecord(record2, 1);
+    assert.deepEqual(1, store1.records["Controversy"].amount);
+    assert.deepEqual(40, store1.stockValue);
+    
+  });
+
+  it('Can I create an inventory with albums and amounts', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    var record1 = new Record("Sign of the times", "Prince", 10);
+    var record2 = new Record("Controversy", "Prince", 20);
+    record1.addTrack("Gonna be a beautiful night")
+    store1.addRecord(record1, 20);
+    store1.addRecord(record2, 10);
+    assert.deepEqual([ { 'Sign of the times': 20 }, { Controversy: 10 } ], store1.listInventory());
+    
+    
+  });
+
+  it('Can I sell a record', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    var record1 = new Record("Sign of the times", "Prince", 10);
+    var record2 = new Record("Controversy", "Prince", 20);
+    var record3 = new Record("Diamonds and Pearls", "Prince", 5);
+    record1.addTrack("Gonna be a beautiful night")
+    store1.addRecord(record1, 20);
+    store1.addRecord(record2, 2);
+    store1.sellRecord(record2)
+    store1.sellRecord(record2)
+    assert.deepEqual(40, store1.profit);
+    
+    
+  });
+
+  it('Can I make an accurate  financial statement that I can use', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    var record1 = new Record("Sign of the times", "Prince", 10);
+    var record2 = new Record("Controversy", "Prince", 20);
+    var record3 = new Record("Diamonds and Pearls", "Prince", 5);
+    record1.addTrack("Gonna be a beautiful night")
+    store1.addRecord(record1, 20);
+    store1.addRecord(record2, 10);
+    store1.sellRecord(record2);
+    store1.sellRecord(record2);
+    var statement = store1.financialStatement();
+    assert.deepEqual(360, statement["stockValue"]);
+    assert.deepEqual(40, statement["takings"]);
+
+    });
+
+  it('Does my customer constructor work', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    var customer1 = new Customer("Andrew", 50)
+    
+    assert.equal("Andrew", customer1.name);
+    assert.deepEqual(50, customer1.cash);
+
+    });
+
+  it('Can my customer buy a record with datetime', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    var customer1 = new Customer("Andrew", 50)
+    var record1 = new Record("Sign of the times", "Prince", 10);
+    customer1.buyRecord(record1, store1);
+    assert.deepEqual({}, customer1.myRecords);
+
+    });
+
+  it('Customer buys record and autimatically runs sellRecors', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    var customer1 = new Customer("Andrew", 50)
+    var record1 = new Record("Sign of the times", "Prince", 10);
+    store1.addRecord(record1, 20);
+    customer1.buyRecord(record1, store1);
+    var statement = store1.financialStatement();
+    assert.deepEqual(10, statement.takings);
+
+    });
+
+  it('Customer buys record and spends cash', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    var customer1 = new Customer("Andrew", 50)
+    var record1 = new Record("Sign of the times", "Prince", 10);
+    store1.addRecord(record1, 20);
+    customer1.buyRecord(record1, store1);
+    assert.deepEqual(40, customer1.cash);
+
+    });
+
+  it('Customer buys record and spends cash', function(){
+    var store1 = new Store("Selectadisc", "Nottingham")
+    var customer1 = new Customer("Andrew", 50)
+    var record1 = new Record("Sign of the times", "Prince", 10);
+    store1.addRecord(record1, 20);
+    customer1.buyRecord(record1, store1);
+    customer1.getPaid(100);
+    assert.deepEqual(140, customer1.cash);
+
+    });
+
 
 })
